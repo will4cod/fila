5d28d76babe901fa83e45746edfe802cd947512e
guimazzola <cms_mazzola_guilherme@hotmail.com>
2021-04-21 15:48:57 -0300

    ajustando projeto

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// error handler !!!
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
