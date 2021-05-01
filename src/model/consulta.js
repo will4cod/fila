fa952f87348244b98afba1ac580523e84892d923
will4cod <william4fernandes@gmail.com>
2021-05-01 19:08:10 -0300

    Adicionado a tela de resultado pedido

---
+++ src/model/consulta.js
@@ -0,0 +1,50 @@
+const pedidos = require('./dados');
+const { format } = require('date-fns');
+const { json } = require('body-parser');
+
+const consultaDados = {
+    //Retorna toda a lista de pedidos 
+    listaTodosPedidos: function(){
+        const listaPedidos = pedidos;
+        return(
+            pedidos
+        )
+    },//Retorna a lista de pedidos com a data atual
+    listaPedidosDeHoje: function(){
+        const diaAtual = format(new Date(), 'dd/MM/yyyy');
+        const pedidosInvertidos = this.listaTodosPedidos().reverse();
+        const listaPedidos = pedidosInvertidos.filter((pedido)=> {
+            if(pedido.data == diaAtual){
+                return(pedido);
+            }
+        })
+
+        return(listaPedidos);
+    },//busca por numero de pedido na base geral
+    buscaNumeroPedido: function(numPedido){
+        const pedidoEncontrado = this.listaTodosPedidos().find(pedido => pedido.numeroPedido == numPedido)
+        return pedidoEncontrado;
+    },//Retorna um array com os numeros dos pedidos de hoje em andamento
+    pedidosAndamento: function(){
+        const pedidosHoje = this.listaPedidosDeHoje();
+        const pedidos = pedidosHoje.map((pedido) => {
+            if(pedido.status == 'andamento'){
+                return pedido.numeroPedido;
+            }
+        })
+
+        return pedidos;
+
+    },//Retorna a posição da fila 
+    posicaoFila: function(arrayPedidos, pedido){
+        const posicao = arrayPedidos.indexOf(pedido);
+        if(posicao === -1){
+            return -1;
+        }else{
+            return posicao + 1;
+        }
+    }
+}
+
+//console.log(consultaDados.posicaoFila(consultaDados.pedidosAndamento(),8));
+module.exports = consultaDados;
---
+++ src/model/dados.js
@@ -0,0 +1,40 @@
+const pedidos = [
+    {
+        id: 1,
+        itens: ['batata frita', 'coca cola'],
+        numeroPedido: 5,
+        guiche: 1,
+        data: '30/04/2021',
+        hora: '12:12',
+        status: 'andamento',
+    },
+    {
+        id: 2,
+        itens: ['hotdog', 'coca cola'],
+        numeroPedido: 6,
+        guiche: 1,
+        data: '01/05/2021',
+        hora: '12:14',
+        status: 'andamento',
+    },
+    {
+        id: 3,
+        itens: ['hotdog', 'delvalle'],
+        numeroPedido: 7,
+        guiche: 1,
+        data: '01/05/2021',
+        hora: '12:16',
+        status: 'andamento',
+    },
+    {
+        id: 4,
+        itens: ['batata frita cheddar e bacon', 'coca cola'],
+        numeroPedido: 8,
+        guiche: 1,
+        data: '01/05/2021',
+        hora: '12:18',
+        status: 'andamento',
+    }
+]
+
+module.exports = pedidos;
